db4e60e4e4545316aa8fce2278b081e6074a4c79
Matheus da Silva Alves <matheusdasilvaalvesm@gmail.com>
2023-09-22 11:16:01 -0300

    Adicionado UI com pontuação no jogo

--- ProjetoDaNave/Assets/Scripts/GameController.cs
+++ ProjetoDaNave/Assets/Scripts/GameController.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public PlayerController player;
+    Vector3 posicaoInicial;
+    float pontuacao;
+    public Text textoPontuacao;
+    public float divisorPontuacao = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+        posicaoInicial = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        pontuacao = (player.transform.position.z - posicaoInicial.z) / divisorPontuacao;
+        textoPontuacao.text = pontuacao.ToString("0");
         
     }
 
